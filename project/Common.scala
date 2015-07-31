79541b0343f7832360972545638db34f6bf85abe
Zak <pattersonzak@gmail.com>
2015-07-31 12:07:36 -0400

    updated for 0.11.4.
    Signed-off-by: Zak <pattersonzak@gmail.com>

--- project/Common.scala
+++ project/Common.scala
@@ -3,20 +3,18 @@ import Keys._
 
 object Common {
   def version = "master-SNAPSHOT"
-  def playVersion = "2.3.8"
-  def scalaVersion =  "2.11.6"
+  def playVersion = "2.3.9"
+  def scalaVersion =  "2.11.7"
 
   def resolvers = Seq(
     "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
     ,"Sonatype repository" at "https://oss.sonatype.org/content/repositories/releases/"
-  ) 
+  )
 
   def dataDependencies = Seq(
     "org.scalatest" %% "scalatest" % "2.2.4" % "test"
-    ,"org.reactivemongo" %% "reactivemongo" % "0.10.5.0.akka23"
-    ,"org.reactivemongo" %% "play2-reactivemongo" % "0.10.5.0.akka23"
+    ,"org.reactivemongo" %% "reactivemongo" % "0.11.4"
     ,"org.scala-lang.modules" %% "scala-xml" % "1.0.3"
-    ,"com.typesafe.akka" %% "akka-actor" % "2.3.9"
     ,"org.scalaz" %% "scalaz-core" % "7.1.1"
     ,"joda-time" % "joda-time" % "2.7"
   )
